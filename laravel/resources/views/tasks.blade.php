f00cb40b1789f23643308c0b6c908c87e1a927fa
Wakana Yoshizawa <wakanayoshizawa@gmail.com>
2019-01-29 13:29:29 +0900

    view 学校のPC対応

--- laravel/resources/views/tasks.blade.php
+++ laravel/resources/views/tasks.blade.php
@@ -9,7 +9,7 @@
         @include('common.errors')
 
         <!-- 新タスクフォーム -->
-        <form action="/task" method="POST" class="form-horizontal">
+        <form action="./task" method="POST" class="form-horizontal">
             {{ csrf_field() }}
 
             <!-- タスク名 -->
@@ -59,7 +59,7 @@
 
                                 <td>
                                     <!-- 削除ボタン -->
-                                    <form action="/task/{{ $task->id }}" method="POST">
+                                    <form action="./task/{{ $task->id }}" method="POST">
                                         {{ csrf_field() }}
                                         {{ method_field('DELETE') }}
 
